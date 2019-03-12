279abf9162c65cd755688e41f8eb901c9282bc80
pchopinet <pierre.chopinet97@gmail.com>
2019-03-12 14:44:18 +0100

    Add default files

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by chopinet on 26/02/19.
+//
+#include "MainWindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
+
